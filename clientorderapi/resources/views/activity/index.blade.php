b652c9ed7ba33d7fcaab772a104c34a788d79184
Sebastian Rodriguez Cruz <sr1290853@gmail.com>
2025-09-16 12:23:27 -0500

    Consumo del servicio technician y activity
    Se ajusta el index para mostrar datos de objetos anidados

---
+++ clientorderapi/resources/views/activity/index.blade.php
@@ -0,0 +1,55 @@
+@extends('templates.base')
+@section('title', 'Actividades')
+@section('header', 'Actividades')
+@section('content')
+    
+    <div class="row">
+        <div class="col-lg12 mb-4 d-grid-gap-2 d-md-block">
+            <a href="{{ route('causal.create') }}" class="btn btn-primary">Crear</a>
+        </div>
+    </div>
+
+    @include('templates.messages')
+
+    <div class="row">
+        <div class="col-lg-12 mb-4">
+            <table id="table_data" class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Descripción</th>
+                        <th>Horas</th>
+                        <th>Tecnico</th>
+                        <th>Tipo</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($activities as $activity)
+                        <tr>
+                        <td>{{ $activity['id'] }}</td>
+                            <td>{{ $activity['description'] }}</td>
+                            <td>{{ $activity['hours'] }}</td>
+                            <td>{{ $activity['technician']['document'] }} - {{ $activity-['technician']['name'] }}</td>
+                            <td>{{ $activity['type_activity']['description'] }}</td>
+                            <td>
+                                <a href="#" class="btn btn-primary btn-circle btn-sm" title="Editar">
+                                    <i class="far fa-edit"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-circle btn-sm" title="Eliminar">
+                                    <i class="fas fa-trash" onclick="return remove();"></i>
+                                </a>
+                            </td>  
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+
+@endsection
+
+@section('scripts')
+    <script src="{{ asset('js/general.js') }}"></script>    
+@endsection
---
+++ clientorderapi/resources/views/technician/index.blade.php
@@ -0,0 +1,54 @@
+@extends('templates.base')
+@section('title', 'Tecnicos')
+@section('header', 'Tecnicos')
+@section('content')
+    
+    <div class="row">
+        <div class="col-lg12 mb-4 d-grid-gap-2 d-md-block">
+            <a href="{{ route('technician.create') }}" class="btn btn-primary">Crear</a>
+        </div>
+    </div>
+
+    @include('templates.messages')
+
+    <div class="row">
+        <div class="col-lg-12 mb-4">
+            <table id="table_data" class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Documento</th>
+                        <th>Nombre</th>
+                        <th>Especialidad</th>
+                        <th>Teléfono</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($technicians as $technician)
+                        <tr>
+                            <td>{{ $technician["id"] }}</td>
+                            <td>{{ $technician["document"] }}</td>
+                            <td>{{ $technician["name"] }}</td>
+                            <td>{{ $technician["speciality"] }}</td>
+                            <td>{{ $technician["phone"] }}</td>
+                            <td>
+                                <a href="{{ route('technician.edit', $technician["id"]) }}" class="btn btn-primary btn-circle btn-sm" title="Editar">
+                                    <i class="far fa-edit"></i>
+                                </a>
+                                <a href="{{ route('technician.destroy', $technician["id"]) }}" class="btn btn-danger btn-circle btn-sm" title="Eliminar">
+                                    <i class="fas fa-trash" onclick="return remove();"></i>
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+
+@endsection
+
+@section('scripts')
+ <script src="{{ asset('js/general.js') }}"></script>    
+@endsection
