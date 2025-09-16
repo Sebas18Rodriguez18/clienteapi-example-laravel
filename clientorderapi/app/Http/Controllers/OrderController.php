0ef0fa570c764f1c2c04416a14f798f3660fda01
Sebastian Rodriguez Cruz <sr1290853@gmail.com>
2025-09-16 12:31:49 -0500

    Consumo del servicio order P1
    falta terminar el método edit donde se consulten las actividades disponibles y actividades

--- clientorderapi/app/Http/Controllers/OrderController.php
+++ clientorderapi/app/Http/Controllers/OrderController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Session;
 
 class OrderController extends Controller
 {
@@ -11,7 +14,13 @@ class OrderController extends Controller
      */
     public function index()
     {
-        //
+        $url = env('URL_BASE_API',"http://localhost:8000");
+            $response = Http::acceptJson()->withToken(Session::get('token'))->get($url . '/order');
+            if($response->successful())
+            {
+                $orders = $response->json();
+                return view('order.index', compact('orders'));
+            } 
     }
 
     /**
@@ -19,7 +28,19 @@ class OrderController extends Controller
      */
     public function create()
     {
-        //
+        $url = env('URL_BASE_API',"http://localhost:8000");
+        $responseTechnicians = Http::acceptJson()->withToken(Session::get('token'))->get($url . '/technician');
+        $responseTypes = Http::acceptJson()->withToken(Session::get('token'))->get($url . '/type_order');
+        if($responseTechnicians->successful() and $responseTypes->successful()){
+
+            $technicians = $responseTechnicians->json();
+            $types = $responseTypes->json();
+            return view('order.create',compact('observations','causals'));
+        }
+        else
+        {
+            abort($responseTechnicians->status());
+        }    
     }
 
     /**
@@ -27,7 +48,30 @@ class OrderController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $url = env('URL_BASE_API',"http://localhost:8000");
+            $response = Http::acceptJson()->withToken(Session::get('token'))->post($url . '/order',[
+                'legalization_date' => $request->legalization_date,
+                'city' => $request->city,
+                'address' => $request->address,
+                'causal_id' => $request->causal_id,
+                'observation_id' => $request->observation_id
+            ]);
+
+            if($response->successful()){
+                session()->flash('message','Registro creado exitosamente');
+                return redirect()->route('order.index');
+
+            }
+            elseif($response->status() == Response::HTTP_BAD_REQUEST)
+            {
+                $errors = $response->json()['errors'];
+                return redirect()->route('order.create')
+                ->withInput()->withErrors($errors);
+            } 
+            else
+            {
+                abort($response->status());
+            }
     }
 
     /**
@@ -43,7 +87,30 @@ class OrderController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $url = env('URL_BASE_API',"http://localhost:8000");
+            $response = Http::acceptJson()->withToken(Session::get('token'))->get($url . '/order/'. $id);
+
+        if($response->successful())
+            {
+                $responseObservations = Http::acceptJson()->withToken(Session::get('token'))->get($url . '/observation');
+                $responseCausals = Http::acceptJson()->withToken(Session::get('token'))->get($url . '/causal');
+                if($responseObservations->successful() and $responseCausals->successful()){
+                $observations = $responseObservations->json();
+                $causals = $responseCausals->json();   
+                $order = $response->json();
+                return view('order.edit', compact('order','observations','causals'));
+            }
+        }
+            elseif($response->status() == Response::HTTP_BAD_REQUEST)
+            {
+                $errors = $response->json()['errors'];
+                return redirect()->route('order.index')
+                ->withInput()->withErrors($errors);
+            } 
+            else
+            {
+                abort($response->status());
+            }
     }
 
     /**
@@ -51,7 +118,33 @@ class OrderController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $url = env('URL_BASE_API',"http://localhost:8000");
+            $response = Http::acceptJson()->withToken(Session::get('token'))->put($url . '/order/'. $id,[
+                'id' => $request->$id,
+                'legalization_date' => $request->legalization_date,
+                'city' => $request->city,
+                'address' => $request->address,
+                'causal_id' => $request->causal_id,
+                'observation_id' => $request->observation_id
+
+
+            ]);
+
+            if($response->successful()){
+                session()->flash('message','Registro actualizado exitosamente');
+                return redirect()->route('order.index');
+
+            }
+            elseif($response->status() == Response::HTTP_BAD_REQUEST)
+            {
+                $errors = $response->json()['errors'];
+                return redirect()->route('order.edit')
+                ->withInput()->withErrors($errors);
+            } 
+            else
+            {
+                abort($response->status());
+            }
     }
 
     /**
@@ -59,6 +152,23 @@ class OrderController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $url = env('URL_BASE_API',"http://localhost:8000");
+            $response = Http::acceptJson()->withToken(Session::get('token'))->delete($url . '/order/'.$id);
+
+            if($response->successful()){
+                session()->flash('message','Registro eliminado exitosamente');
+                return redirect()->route('order.index');
+
+            }
+            elseif($response->status() == Response::HTTP_BAD_REQUEST)
+            {
+                $errors = $response->json()['errors'];
+                return redirect()->route('order.index')
+                ->withInput()->withErrors($errors);
+            } 
+            else
+            {
+                abort($response->status());
+            }
     }
 }
